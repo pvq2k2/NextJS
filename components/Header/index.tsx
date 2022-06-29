f849bacf937f72374fcf373bec70ad69563cd326
pvq2k2 <vanquyet2392002e@gmail.com>
2022-06-29 22:02:11 +0700

    feat: Day 1 - Header Component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,4 @@
+import styles from './header.module.scss';
 import Link from 'next/link'
 import React from 'react'
 
@@ -5,20 +6,20 @@ type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <>
+    <div className={styles.header}>
     <h1>Header</h1>
-    <ul>
-        <li>
-            <Link href="">Home</Link>
+    <ul className={styles.menu}>
+        <li className={styles.menu__link}>
+            <Link href="/">Home</Link>
         </li>
-        <li>
+        <li className={styles.menu__link}>
             <Link href="about">About</Link>
         </li>
-        <li>
+        <li className={styles.menu__link}>
             <Link href="product">Product</Link>
         </li>
     </ul>
-</>
+</div>
   )
 }
 
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import Header from 'components/Header/index';
 type Props = {}
 
 const About = (props: Props) => {
   return (
+    <>
+    <Header />
     <div className='bg-black'>About</div>
+    </>
+
   )
 }
 
